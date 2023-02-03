fa04b73c3da84b3ef9c81841b59395c672ee53f2
jeremtrue <jeremyptruelove788@gmail.com>
2023-02-02 21:34:16 -0800

    background revamp

--- script.js
+++ script.js
@@ -129,26 +129,6 @@ function insertLetter(pressedKey) {
   currentGuess.push(pressedKey);
   nextLetter += 1;
 }
-/*BELOW THIS IS THE ISSUE
-const animateCSS = (element, animation, prefix = "animate__") =
-  new Promise((resolve, reject) => {
-    const animationName = `${prefix}${animation}`;
-    // const node = document.querySelector(element);
-    const node = element;
-    node.style.setProperty("--animate-duration", "0.3s");
-
-    node.classList.add(`${prefix}animated`, animationName);
-
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve("Animation ended");
-    }
-
-    node.addEventListener("animationend", handleAnimationEnd, { once: true });
-  });
-*/
 document.addEventListener("keyup", (e) => {
   if (guessesRemaining === 0) {
     return;
