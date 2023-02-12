e8d09c1d60936f5e00f1b69e9cba5305acabccb6
jeremtrue <jeremyptruelove788@gmail.com>
2023-02-12 12:44:53 -0800

    starting to create the leaderboard

---
+++ leaderboard/leader.js
@@ -0,0 +1,16 @@
+//creating player variables
+let player1score = 0 ;
+let player2score = 0 ;
+let player3score = 0 ;
+let player4score = 0 ;
+let player5score = 0 ;
+//loggging player scores for debuging purposes.
+console.log(player1score);
+console.log(player2score);
+console.log(player3score);
+console.log(player4score);
+console.log(player5score);
+
+function submitscore(){
+    
+}
