97cc31ec612d7b4eebf36356bcbcbbae1aefd2cf
jeremtrue <jeremyptruelove788@gmail.com>
2023-02-15 15:07:51 -0700

    SOMWHERE

--- leaderboard/leader.js
+++ leaderboard/leader.js
@@ -4,6 +4,15 @@ let player2score = 0 ;
 let player3score = 0 ;
 let player4score = 0 ;
 let player5score = 0 ;
+
+//creating cookies to store leaderboard information.
+
+sessionStorage.setItem(player1score, player1score);
+sessionStorage.setItem(player1score, player2score);
+sessionStorage.setItem(player1score, player3score);
+sessionStorage.setItem(player1score, player4score);
+sessionStorage.setItem(player1score, player5score);
+
 //loggging player scores for debuging purposes.
 console.log(player1score);
 console.log(player2score);
@@ -12,6 +21,13 @@ console.log(player4score);
 console.log(player5score);
 
 function submitscore(){
-    let 
+
 }
 //trying to start a basic submit score script
+
+
+
+
+//https://www.w3schools.com/js/js_output.asp
+//https://www.w3schools.com/html/html_forms.asp
+//
