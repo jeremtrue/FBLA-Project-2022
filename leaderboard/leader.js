0ecb56a3eaa601f8485dcec9eb6d669466874228
jeremtrue <jeremyptruelove7888@gmail.com>
2023-02-25 23:24:53 -0700

    dont work

--- leaderboard/leader.js
+++ leaderboard/leader.js
@@ -7,22 +7,16 @@ let player5score = 0 ;
 
 //creating cookies to store leaderboard information.
 
-sessionStorage.setItem(player1score, player1score);
-sessionStorage.setItem(player1score, player2score);
-sessionStorage.setItem(player1score, player3score);
-sessionStorage.setItem(player1score, player4score);
-sessionStorage.setItem(player1score, player5score);
+localStorage.setItem("player1score", "0");
+localStorage.setItem("player2score", "0");
+localStorage.setItem("player3score", "0");
+localStorage.setItem("player4score", "0");
+localStorage.setItem("player5score", "0");
 
 //loggging player scores for debuging purposes.
-console.log(player1score);
-console.log(player2score);
-console.log(player3score);
-console.log(player4score);
-console.log(player5score);
+document.getElementById("player1").innerHTML = localStorage.getItem("player1score");
 
-function submitscore(){
 
-}
 //trying to start a basic submit score script
 
 
