df10ba32b1e71f4d35b25569cd39b55c4b81bb75
jeremtrue <jeremyptruelove7888@gmail.com>
2023-03-26 20:58:08 -0700

    battery ab to die

--- leaderboard/leader.js
+++ leaderboard/leader.js
@@ -1,17 +1,18 @@
-//creating player variables
-let player1score = 9 ;
-let player2score = 0 ;
-let player3score = 0 ;
-let player4score = 0 ;
-let player5score = 0 ;
 
 //creating cookies to store leaderboard information.
 
-localStorage.setItem("player1score", player1score);
-localStorage.setItem("player2score", player2score);
-localStorage.setItem("player3score", player3score);
-localStorage.setItem("player4score", player4score);
-localStorage.setItem("player5score", player5score);
+localStorage.setItem("player1score", "");
+localStorage.setItem("player2score", "");
+localStorage.setItem("player3score", "");
+localStorage.setItem("player4score", "");
+localStorage.setItem("player5score", "");
+
+//creating player variables
+let p1score = localStorage.getItem("player1score") ;
+//let player2score = 0 ;
+//let player3score = 0 ;
+//let player4score = 0 ;
+//let player5score = 0 ;
 
 //loggging player scores for debuging purposes.
 //document.getElementById("player1").innerHTML = localStorage.getItem("player1score");
@@ -24,4 +25,10 @@ localStorage.setItem("player5score", player5score);
 
 //https://www.w3schools.com/js/js_output.asp
 //https://www.w3schools.com/html/html_forms.asp
-//
+
+
+document.getElementById("leaderboard").innerHTML += "Player 1:" + p1score ;
+/*document.getElementById("leaderboard").innerHTML += 'Player 1:';
+document.getElementById("leaderboard").innerHTML += 'Player 1:';
+document.getElementById("leaderboard").innerHTML += 'Player 1:';
+*/
